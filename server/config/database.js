67eb344f662f0b8cd46caa07df336267b93ccc04
Lasith Manujitha <Manujitha98@gmail.com>
2023-12-04 00:18:35 +0530

    Add database connection

---
+++ server/config/database.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+async function connectDB() {
+	try {
+		await mongoose.connect("mongodb://127.0.0.1:27017/mern-crud");
+		console.log("MongoDB connected");
+	} catch (error) {
+		console.log(error.message);
+	}
+}
+
+module.exports = connectDB;
