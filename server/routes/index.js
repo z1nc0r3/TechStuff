8e3b30a02f4630eac46da4e170562e78a080e1e5
Lasith Manujitha <Manujitha98@gmail.com>
2023-12-05 19:16:28 +0530

    Code refactoring

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,13 +4,24 @@ const userController = require('../controllers/userController.js');
 const articleController = require('../controllers/articleController.js');
 
 router.get('/', articleController.getArticles);
+
+// Login routes
 router.get('/login', userController.login);
 router.post('/login', userController.loginHandler);
+
+// Register routes
 router.get('/register', userController.register);
 router.post('/register', userController.registerHandler);
+
+// Logout route
 router.get('/logout', userController.logout);
+
+// Account route
 router.get('/account', userController.account);
-// router.get('/update', userController.update);
+
+// Account actions
+router.get('/update', userController.update);
+router.post('/update', userController.updateHandler);
 router.post('/user/delete', userController.deleteUser);
 
 module.exports = router;
