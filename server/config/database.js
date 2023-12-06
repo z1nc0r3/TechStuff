ec6fb6ef38f1f9e9b66d65794d2e4705099d8377
Lasith Manujitha <Manujitha98@gmail.com>
2023-12-06 22:08:45 +0530

    Update database logic

--- server/config/database.js
+++ server/config/database.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 async function connectDB() {
 	try {
-		await mongoose.connect("mongodb://127.0.0.1:27017/mern-crud");
+		await mongoose.connect("mongodb://127.0.0.1:27017/mern-crud", {
+			autoCreate: true,
+		});
 		console.log("MongoDB connected");
 	} catch (error) {
 		console.log(error.message);
